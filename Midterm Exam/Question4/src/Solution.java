ed62f96c8ff3ab672339027eacae1375d38c52bd
Zhening Feng <fengzhening1116@outlook.com>
2021-04-01 20:23:43 -0700

    update

--- Midterm Exam/Question4/src/Solution.java
+++ Midterm Exam/Question4/src/Solution.java
@@ -7,16 +7,22 @@ public class Solution {
         int[] nums = {2,4,4,4,6,7,8,9,9,9,10,11,12,13,13,15,15};
         System.out.println(Arrays.toString(nums));
         System.out.println("Index of 7 is " + getIndex(nums,7));
+        System.out.println("Index of 10 is " + getIndex(nums,10));
         System.out.println("Index of 2 is " + getIndex(nums,2));
         System.out.println("Index of 15 is " + getIndex(nums,15));
-
+        System.out.println("Index of 100 is " + getIndex(nums,100));
+        System.out.println();
+        int[] fives= {5,5,5,5,5};
+        System.out.println(Arrays.toString(fives));
+        System.out.println("Index of 5 is: " + getIndex(fives,5));
     }
 
-    // Time Complexity O(log(n)) we use binary search
+    // Time Complexity O(log(n)) we used binary search to speed up the search
     //Space Complexity O(1) we did not use extra space to store data
     public static int getIndex(int[] nums, int x){
         // IF not existed in this array, return -1;
         if(x > nums[nums.length -1] || x < nums[0]){
+            System.out.print("(The x is not in this array)");
             return -1;
         }
 
